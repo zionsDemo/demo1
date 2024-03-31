88f02eff5ee1ed9618a776abf451c16f8d28731b
gendlee <gendlee@sina.com>
2024-03-31 17:37:42 +0800

    chap3

--- src/main/java/chap3/JavaDataType.java
+++ src/main/java/chap3/JavaDataType.java
@@ -2,7 +2,17 @@ package chap3;
 
 public class JavaDataType {
     public static void main(String[] args) {
+        byte myByte = 99;          // byte 类型
+        short myShort = 100;       // short 类型
+        int myInt = 101;           // int 类型
+        long mylong = 102;         // long 类型
+        float myFloat = 102.99f;   // float 类型
+        double myDouble = 103.99;  // double 类型
+        boolean myBoolean = true;  // boolean 类型
+        char myChar = 'Z';         // char 类型
 
+        String myString = "Hi";    // String 类型
+        String[] myArrays = new String[]{"Hi", "Java"};     // String数组类型
     }
 
 }

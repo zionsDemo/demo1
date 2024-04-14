e0bf73f08293539e1b83c83e40a37bf21f66d8fa
gendlee <gendlee@sina.com>
2024-04-14 12:43:20 +0800

    Addition

--- src/main/java/chap3/Addition.java
+++ src/main/java/chap3/Addition.java
@@ -2,8 +2,15 @@ package chap3;
 
 public class Addition {
     public static void main(String[] args) {
-        int myInt = 101;                  // myInt赋值101
-        int sum = myInt + 100;         // myInt加100
-        System.out.println(sum);     // 输出 201
+        // 代码片段1
+        int myInt = 101;               // myInt赋值101
+        int sum = myInt + 100;        // myInt加100
+        System.out.println(sum);      // 输出 201
+
+        // 代码片段2
+        myInt += 100;                    // myInt加100
+        System.out.println(myInt);     // 输出 201
+
+
     }
 }

060ce9937f0d7cf1db084bf7e3ebdf3cf76b23ea
gendlee <gendlee@sina.com>
2024-04-14 12:32:09 +0800

    加法

---
+++ src/main/java/chap3/Addition.java
@@ -0,0 +1,9 @@
+package chap3;
+
+public class Addition {
+    public static void main(String[] args) {
+        int myInt = 101;                  // myInt赋值101
+        int sum = myInt + 100;         // myInt加100
+        System.out.println(sum);     // 输出 201
+    }
+}
